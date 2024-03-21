e7032b9b80e84b96ca36fd3a5bc8140047c07465
BenardSilomaMasikond <BenardSilomaMasikond@JITU-PC110VYA>
2024-03-21 15:14:02 +0300

    added a failing test

--- cypress-tests/cypress/e2e/spec.cy.js
+++ cypress-tests/cypress/e2e/spec.cy.js
@@ -6,4 +6,13 @@ describe('cypress works!', () => {
     
     cy.get(".text-2xl").contains("Students List")
   })
+
+
+  it('Student List will not be found and will fail', () => {
+    cy.wait(4000)
+    cy.visit('/');
+    
+    
+    cy.get(".text-wjkwjw2xl").contains("Students List")
+  })
 })
